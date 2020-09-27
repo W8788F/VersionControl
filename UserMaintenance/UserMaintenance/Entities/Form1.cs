346a556e97fb7141b00af274a9842c0110e6867c
W8788F <gergo08@gmail.com>
2020-09-27 12:29:55 +0200

    fullname

--- UserMaintenance/UserMaintenance/Entities/Form1.cs
+++ UserMaintenance/UserMaintenance/Entities/Form1.cs
@@ -23,7 +23,7 @@ namespace UserMaintenance.Entities
                 InitializeComponent();
 
                 label1.Text = Resource1.LastName;
-                label2.Text = Resource1.FirstName;
+                //label2.Text = Resource1.FirstName;
                 button1.Text = Resource1.Add;
 
 
@@ -39,8 +39,8 @@ namespace UserMaintenance.Entities
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text,
+                FullName = textBox1.Text,
+
             };
             users.Add(u);
         }
