ccccc26476a7593e99d35f7ff2eb29c8c180b3fa
W8788F <gergo08@gmail.com>
2020-09-27 12:38:33 +0200

    fajlba iras

--- UserMaintenance/UserMaintenance/Entities/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Entities/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -87,11 +88,21 @@
             this.textBox2.Size = new System.Drawing.Size(158, 26);
             this.textBox2.TabIndex = 5;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 51);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Fájlba írás";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
@@ -113,5 +124,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
     }
 }
