bdfd9df442081de866d495f27352ecced5ac434b
W8788F <gergo08@gmail.com>
2020-10-04 19:21:28 +0200

    8. feladat

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -80,6 +80,18 @@ namespace week4
 
         }
 
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
         private string GetCell(int x, int y)
         {
 
