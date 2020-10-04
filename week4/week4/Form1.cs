1ba6454efaf844f0ef7eec5a9016e6b4f064d6af
W8788F <gergo08@gmail.com>
2020-10-04 10:24:49 +0200

    6.feladat

--- week4/week4/Form1.cs
+++ week4/week4/Form1.cs
@@ -16,20 +16,57 @@ namespace week4
 {
 
 
+
     public partial class Form1 : Form
     {
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+        
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
         public Form1()
         {
+            CreateExcel();
             InitializeComponent();
             LoadData();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {      
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                //CreateTable(); 
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
         private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+
     }
 }
