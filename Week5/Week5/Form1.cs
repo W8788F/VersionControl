5c8de366d699e324bd67691563c1d5cf9a5ec8c0
W8788F <gergely.mate@stud.uni-corvinus.hu>
2020-10-10 18:44:53 +0200

    5.feladat

--- Week5/Week5/Form1.cs
+++ Week5/Week5/Form1.cs
@@ -12,6 +12,7 @@ namespace Week5
 {
     public partial class Form1 : Form
     {
+      
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         public Form1()
@@ -19,6 +20,8 @@ namespace Week5
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+       
         }
+
     }
 }
