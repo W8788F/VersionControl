196d212bfde53574426c5b4851cbab8f6cfe806f
W8788F <gergely.mate@stud.uni-corvinus.hu>
2020-10-25 09:29:36 +0100

    3.feladat

---
+++ week7/week07/week07/Entities/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+
+}
