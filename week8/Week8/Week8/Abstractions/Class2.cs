125b76e26c1146afd391eb5ec508e72400e00dd9
W8788F <gergely.mate@stud.uni-corvinus.hu>
2020-11-15 10:29:30 +0100

    9. feladat

--- week8/Week8/Week8/Abstractions/Class2.cs
+++ week8/Week8/Week8/Abstractions/Class2.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Week8.Entities;
 
 namespace Week8.Abstractions
 {
     interface IToyFactory
     {
         Toy CreateNew();
+    }
+
+    public class BallFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+    }
+
+    public class CarFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
     }
 }
